5b4406ebcff44937c325958c1f86e44952bd2a56
Vikas <vikasrao24@gmail.com>
2023-11-07 03:11:58 +0530

    third added

---
+++ modules/resource_groups/variables.tf
@@ -0,0 +1,29 @@
+# Variables
+variable "prefix" {
+  type        = string
+  description = "resource prefix"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure Region"
+}
+
+variable "environment" {
+  description = "environment"
+  type        = string
+}
+
+variable "ask_id" {
+  description = "ask id of the project"
+  type        = string
+}
+
+variable "projectname" {
+  description = "name of the project"
+  type        = string
+}
+variable "description" {
+  description = "name of the project"
+  type        = string
+}
