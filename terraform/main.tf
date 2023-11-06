6ab094be44a3270e81b32bd0147389664301e764
Vikas <vikasrao24@gmail.com>
2023-11-06 18:26:21 +0530

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+module "resource_groups" {
+  source = "../modules/resource_groups"
+  location = var.location
+  prefix = var.prefix
+  environment = var.environment
+  projectname = var.projectname
+  ask_id = var.ask_id
+  description = "this is resource group"
+  }
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,16 @@
+###Prefix
+prefix = "actionsdev"
+
+
+###location
+location ="Central US"
+
+###Environment
+environment = "nonprod"
+
+###Ask Id
+ask_id = "AIDE_00000"
+
+###Project Name
+projectname = "ocare"
+
