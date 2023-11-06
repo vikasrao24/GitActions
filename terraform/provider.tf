b758521ff79698ebb5d72beb45e001454be125ac
Vikas <vikasrao24@gmail.com>
2023-11-07 03:23:55 +0530

    provider modifid

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,14 @@
-#Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicrop/azurerm"
-      version = "3.74.0"
+      source = "hashicorp/azurerm"
+      version = "3.79.0"
     }
   }
 }
 
-# Azure provider configuration
 provider "azurerm" {
-  features {}
+  features { 
+  }
+  # Configuration options
 }
